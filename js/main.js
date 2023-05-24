d64c1290efe27bd492bbf5e85f6593947ada3428
Bill Cabrera <bill@chubbycrowrecords.com>
2023-05-24 19:11:54 -0400

    get rid of resize issue when error message is displayed; clean up js

--- js/main.js
+++ js/main.js
@@ -3,41 +3,29 @@ const email = document.getElementById("emailaddress");
 const emailError = document.querySelector("span.error");
 
 email.addEventListener("input", (event) => {
-  // check each time the user types something
-
-  if (email.validity.valid) {
-    // if error message is visible but entry is valid, remove error message.
-    emailError.textContent = ""; 		// clear content of message
-    emailError.className = "error"; 	// reset visual state of message
-  } else {
-    // if still an error, display the correct error
-    showError();
-  }
+	if (email.validity.valid) {
+		// if error message is visible but entry is valid, remove error message
+		emailError.textContent = "";
+		emailError.className = "error"; // reset visual state of message
+	} else {
+		showError();
+	}
 });
 
 form.addEventListener("submit", (event) => {
-  // if email field is valid, let the form submit
-  if (!email.validity.valid) {
-    // if not valid, display appropriate error message
-    showError();
-    // prevent form from being sent by canceling the event
-    event.preventDefault();
-  }
+	if (!email.validity.valid) {
+		showError();
+		// prevent form from being sent by canceling event
+		event.preventDefault();
+	}
 });
 
 function showError() {
-  if (email.validity.valueMissing) {
-    // If the field is empty,
-    emailError.textContent = "Please provide a valid email";
-  } else if (email.validity.typeMismatch) {
-    // If the field doesn't contain an email address
-    emailError.textContent = "Please provide a valid email";
-  } //else if (email.validity.tooShort) {
-    // If the data is too short,
-    // display the following error message.
-  // emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-  //}
+	if (email.validity.valueMissing) {
+		emailError.textContent = "Please provide a valid email";
+	} else if (email.validity.typeMismatch) {
+		emailError.textContent = "Please provide a valid email";
+	}
 
-  // Set the styling appropriately
-  emailError.className = "error active";
+	emailError.className = "error active";
 }
